9f1380a3c1514fb2929dd6c4e7ec6ea43a458f2c
Jay R Bolton <jayrbolton@gmail.com>
2019-02-05 11:15:06 -0800

    Log json parsing errors for non-json responses; add a RETRY_ERRORS env var option

--- src/import_workspaces.py
+++ src/import_workspaces.py
@@ -6,7 +6,7 @@ import os
 
 
 def req_import(wsid):
-    return requests.post(
+    resp = requests.post(
         'https://kbase.us/dynserv/bbf73eeab74478490e0f8c8225f69664862da587.relation-engine-sync',  # noqa
         data=json.dumps({
             'method': 'import_range',
@@ -15,13 +15,18 @@ def req_import(wsid):
                 'stop': wsid + 1
             }
         })
-    ).json()
+    )
+    try:
+        return resp.json()
+    except Exception:
+        return {'error': resp.text}
 
 
 if __name__ == '__main__':
     db_statuses = plyvel.DB('db/workspace_statuses', create_if_missing=False)
     db_errs = plyvel.DB('db/workspace_errors', create_if_missing=False)
     max_count = int(os.environ.get('MAX_COUNT', 0))
+    retry_errors = os.environ.get('RETRY_ERRORS', False)
     count = 0
     for key, status in db_statuses:
         if max_count and count >= max_count:
@@ -30,6 +35,10 @@ if __name__ == '__main__':
         wsid = int.from_bytes(key, byteorder='big')
         if status == b'completed':
             print(f'skipping {wsid}')
+            continue
+        # Skip wsids that previously error'd if retry_errors is false
+        if not retry_errors and status == b'error':
+            print(f'error on {wsid}')
             continue
         resp = req_import(wsid)
         if resp.get('error'):
