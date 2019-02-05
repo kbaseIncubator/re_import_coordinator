165b539b4337675537bca8b6799d15cda7cdef34
Jay R Bolton <jayrbolton@gmail.com>
2019-02-05 10:06:44 -0800

    Add a max count option using an env var

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 
 ```sh
 $ pipenv install
-$ pipenv run src/import_workspaces.py
+$ MAX_COUNT=100 pipenv run src/import_workspaces.py
 ```
+
+`MAX_COUNT` is the max number of pending workspaces you want to try to import. If you dont set it, then it will go through all workspaces.
--- src/import_workspaces.py
+++ src/import_workspaces.py
@@ -2,10 +2,7 @@ import plyvel
 import json
 import requests
 import sys
-
-# file that saves all pending workspaces
-# move workspace id from pending file to success or error file
-# save the error
+import os
 
 
 def req_import(wsid):
@@ -24,16 +21,23 @@ def req_import(wsid):
 if __name__ == '__main__':
     db_statuses = plyvel.DB('db/workspace_statuses', create_if_missing=False)
     db_errs = plyvel.DB('db/workspace_errors', create_if_missing=False)
+    max_count = int(os.environ.get('MAX_COUNT', 0))
+    count = 0
     for key, status in db_statuses:
+        if max_count and count >= max_count:
+            print(f'hit max count of {max_count} exiting')
+            sys.exit(0)
         if status == b'completed':
             print('skipping..')
             continue
         wsid = int.from_bytes(key, byteorder='big')
         resp = req_import(wsid)
         if resp.get('error'):
+            print(f'error on {wsid}')
             db_statuses.put(key, b'error')
             db_errs.put(key, str(resp['error'])[0:512].encode())
         else:
+            print(f'completed {wsid}')
             db_statuses.put(key, b'completed')
             db_errs.delete(key)
-        sys.exit(1)  # TODO continue
+        count += 1
